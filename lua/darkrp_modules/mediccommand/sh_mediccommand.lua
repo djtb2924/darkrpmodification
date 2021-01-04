4ca1dcb28c27001b44ec4426d4645f2b37188486
Théo BAUDRU <contact@gasp.dev>
2021-01-04 19:47:34 +0100

    Add medic module

---
+++ lua/darkrp_modules/mediccommand/sh_mediccommand.lua
@@ -0,0 +1,7 @@
+-- Copyright © LiveYourGame, 2021
+
+DarkRP.declareChatCommand{
+    command = "medic",
+    description = "Message aux médecins du samu",
+    delay = 1.5
+}
---
+++ lua/darkrp_modules/mediccommand/sv_mediccommand.lua
@@ -0,0 +1,24 @@
+local green = Color(20, 150, 20)
+
+local function medic(ply, args)
+    return args, function(text)
+        if text == "" then
+            DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", DarkRP.getPhrase("arguments"), ""))
+            return
+        end
+
+        if not team.NumPlayers(TEAM_SAMU) then return end
+
+        local col = team.GetColor(ply:Team())
+        local name = ply:Nick()
+
+        local players = player.GetAll()
+        for i = 1, #players do
+            if players[i]:Team() == TEAM_SAMU or players[i] == ply then
+                DarkRP.talkToPerson(players[i], col, "(Dépannage) " .. name, green, text, ply)
+            end
+        end
+    end
+end
+
+DarkRP.defineChatCommand("medic", medic, 1.5)
